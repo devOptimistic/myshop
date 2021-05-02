9a404ef17b16efd69b2028bb596192ccc32104e5
devOptimistic <79414784+devOptimistic@users.noreply.github.com>
2021-05-02 18:30:26 +0430

    Delete public directory

--- public/catalog.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "__php__.php";
-
-$db=new DB();
-$table=Product::find();
-unset($db);
-$alerts = alerts();
-view('catalog', null, ['table' => $table], $alerts);
-?>
--- public/messages.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "__php__.php";
-
-$db=new DB();
-$table=Contact::find();
-unset($db);
-$alerts = alerts();
-view('messages', null, ['table' => $table], $alerts);
-?>
