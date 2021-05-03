957b4cdf2f325bec4770fa60f9d41a494f4f74de
devOptimistic <79414784+devOptimistic@users.noreply.github.com>
2021-05-03 12:56:48 +0430

    Update Authentication.php

--- includes/security/Authentication.php
+++ includes/security/Authentication.php
@@ -4,6 +4,7 @@ if(! class_exists('Authentication')){
         static public function login($uid){
             //$_SESSION['authenticated']=true;
             $_SESSION['uid']=$uid;
+            $_SESSION['lastLogin']=time();
         }
         static public function check(){
             return isset($_SESSION['uid']);
@@ -13,6 +14,15 @@ if(! class_exists('Authentication')){
         }
         static public function uid(){
             return $_SESSION['uid'];
+        }
+        static public function autoLogout(){
+            if(time() - $_SESSION['lastLogin'] > loginDeadline*24*60*60) //  30 Days
+                self::logout();
+            if(time() - $_SESSION['lastActivity'] > activityDeadline * 60) //  10 Mins
+                self::logout();
+
+            $_SESSION['lastActivity']=time();
+
         }
     }
 }
